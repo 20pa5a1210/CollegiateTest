479e90e9db066529143ef42c10e30bf77bd1478e
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-08-13 14:25:04 +0530

    graphql typescript setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+import resolvers from "./Resolvers/resolver";
+import { typeDefs } from "./Schema/typedefs";
+
+const startServer = async () => {
+
+    const server = new ApolloServer({ typeDefs, resolvers });
+    const { url } = await startStandaloneServer(server, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+        listen: { port: 4000 },
+    });
+
+    console.log(`🚀  Server ready at ${url}`);
+}
+
+startServer();
